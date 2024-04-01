a9885c46cb23ab7b0f0fd482ff5b48ee42c5e51d
Delweng <delweng@gmail.com>
2024-04-01 20:53:56 +0800

    eth/tracers: fix base fee and set blob fee in tests (#29376)
    Signed-off-by: jsvisa <delweng@gmail.com>
    Co-authored-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- eth/tracers/internal/tracetest/prestate_test.go
+++ eth/tracers/internal/tracetest/prestate_test.go
@@ -92,17 +92,8 @@ func testPrestateDiffTracer(tracerName string, dirPath string, t *testing.T) {
 			}
 			// Configure a blockchain with the given prestate
 			var (
-				signer  = types.MakeSigner(test.Genesis.Config, new(big.Int).SetUint64(uint64(test.Context.Number)), uint64(test.Context.Time))
-				context = vm.BlockContext{
-					CanTransfer: core.CanTransfer,
-					Transfer:    core.Transfer,
-					Coinbase:    test.Context.Miner,
-					BlockNumber: new(big.Int).SetUint64(uint64(test.Context.Number)),
-					Time:        uint64(test.Context.Time),
-					Difficulty:  (*big.Int)(test.Context.Difficulty),
-					GasLimit:    uint64(test.Context.GasLimit),
-					BaseFee:     test.Genesis.BaseFee,
-				}
+				signer             = types.MakeSigner(test.Genesis.Config, new(big.Int).SetUint64(uint64(test.Context.Number)), uint64(test.Context.Time))
+				context            = test.Context.toBlockContext(test.Genesis)
 				triedb, _, statedb = tests.MakePreState(rawdb.NewMemoryDatabase(), test.Genesis.Alloc, false, rawdb.HashScheme)
 			)
 			defer triedb.Close()
