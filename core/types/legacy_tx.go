3223e2867b0cd97836e0743704a58cc5a3afac7f
Gilad Haimov <giladha@hotmail.com>
2024-06-17 23:23:39 +0300

    more formatting

--- core/types/legacy_tx.go
+++ core/types/legacy_tx.go
@@ -24,14 +24,14 @@ import (
 
 // LegacyTx is the transaction data of regular Ethereum transactions.
 type LegacyTx struct {
-	Nonce        uint64          // nonce of sender account
-	GasPrice     *big.Int        // wei per gas
-	Gas          uint64          // gas limit
-	To           *common.Address `rlp:"nil"` // nil means contract creation
-	Value        *big.Int        // wei amount
-	Data         []byte          // contract invocation input data
-	V, R, S      *big.Int        // signature values
-	OrigGasPrice *big.Int        `rlp:"optional"` //@lfm
+	Nonce    uint64          // nonce of sender account
+	GasPrice *big.Int        // wei per gas
+	Gas      uint64          // gas limit
+	To       *common.Address `rlp:"nil"` // nil means contract creation
+	Value    *big.Int        // wei amount
+	Data     []byte          // contract invocation input data
+	V, R, S  *big.Int        // signature values
+	OrigGasPrice *big.Int    `rlp:"optional"` //@lfm
 }
 
 // NewTransaction creates an unsigned legacy transaction.
