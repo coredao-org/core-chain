a3e83f96496eda00af1f1d1e79c97dba3df34367
Chris Ziogas <ziogaschr@gmail.com>
2025-02-14 00:18:10 +0200

    file rename

--- consensus/satoshi/lfm_discounts.go
+++ consensus/satoshi/lfm_discounts.go
@@ -60,17 +60,26 @@ func NewLFMDiscountConfigProvider(
 	// Force load on next block
 	provider.discountConfigsReloadOnNextBlock.Store(true)
 
+	// Load the discount configs to warm up the in memory cache
+	// err = provider.loadDiscountConfigs()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to load the discount configs: %w", err)
+	// }
+
 	return provider, nil
 }
 
 // OnBlockStart is called when a new block is started
 func (p *LFMDiscountConfigProvider) OnBlockStart(blockNumber uint64) {
+	fmt.Println("lmf.OnBlockStart discountConfigsReloadOnNextBlock", p.discountConfigsReloadOnNextBlock.Load())
 	if !p.discountConfigsReloadOnNextBlock.Load() {
 		return
 	}
+	fmt.Println("lmf.OnBlockStart discountConfigsReloadOnNextBlock1", blockNumber, "configsBlockNumber", p.configsBlockNumber)
 	p.discountConfigsReloadOnNextBlock.Store(false)
 
 	p.loadDiscountConfigs(blockNumber)
+
 }
 
 // ReloadOnNextBlock marks the discount configs reload on next block
@@ -85,7 +94,10 @@ func (p *LFMDiscountConfigProvider) loadDiscountConfigs(blockNumber uint64) erro
 		return nil
 	}
 
+	fmt.Println("loadDiscountConfigs.rpcBlockNumber", blockNumber, rpc.BlockNumber(blockNumber))
 	rpcBlockNumber := rpc.BlockNumberOrHashWithNumber(rpc.BlockNumber(blockNumber))
+	fmt.Println("loadDiscountConfigs.rpcBlockNumber1", rpcBlockNumber)
+
 	// method := "discountConfigs"
 	method := "getAllAvailableDiscountConfigs"
 
@@ -100,6 +112,8 @@ func (p *LFMDiscountConfigProvider) loadDiscountConfigs(blockNumber uint64) erro
 		return err
 	}
 
+	fmt.Println("start fetch")
+
 	// Call the system contract
 	msgData := (hexutil.Bytes)(data)
 	toAddress := common.HexToAddress(systemcontracts.LFMDiscountContract)
@@ -109,6 +123,8 @@ func (p *LFMDiscountConfigProvider) loadDiscountConfigs(blockNumber uint64) erro
 		To:   &toAddress,
 		Data: &msgData,
 	}, rpcBlockNumber, nil, nil)
+
+	fmt.Println("end fetch")
 	if err != nil {
 		log.Error("Failed to fetch discount contract configs", "error", err)
 		return err
@@ -138,6 +154,7 @@ func (p *LFMDiscountConfigProvider) loadDiscountConfigs(blockNumber uint64) erro
 	for _, config := range configs {
 		if config.IsEOADiscount {
 			eoaToEoaDiscount = config.DiscountRate
+			fmt.Println("eoaToEoaDiscount", config)
 			continue
 		}
 
@@ -151,6 +168,7 @@ func (p *LFMDiscountConfigProvider) loadDiscountConfigs(blockNumber uint64) erro
 			MinimumValidatorShare: config.MinimumValidatorShare,
 		}
 
+		fmt.Println("incoming Discount", config)
 		if !isValidConfig(discountConfig) {
 			log.Debug("Invalid LFM discount config", "address", discountConfig.DiscountAddress, "config", discountConfig)
 			continue
@@ -166,6 +184,12 @@ func (p *LFMDiscountConfigProvider) loadDiscountConfigs(blockNumber uint64) erro
 	p.configsBlockNumber = blockNumber
 	p.lock.Unlock()
 
+	// TODO: remove this
+	log.Info("Loaded LFM discount configs", "count", len(newDiscountConfigs), newDiscountConfigs)
+	for addr, config := range p.discountConfigs {
+		log.Info("LFM discount config", "address", addr, "config", config)
+	}
+
 	return nil
 }
 
@@ -177,9 +201,27 @@ func (p *LFMDiscountConfigProvider) GetEOAToEOADiscount() *big.Int {
 }
 
 func (p *LFMDiscountConfigProvider) GetDiscountConfigByAddress(address common.Address) (config types.LFMDiscountConfig, ok bool) {
+	// // TODO: REMOVE THIS AS IT IS TEMP USED TO ALWAYS RELOAD THE CONFIGS
+	// if err := p.loadDiscountConfigs(); err != nil {
+	// 	log.Debug("Failed to reload discount configs", "error", err)
+	// 	return types.LFMDiscountConfig{}, false
+	// }
+
+	// if len(p.discountConfigs) == 0 {
+	// 	if err := p.loadDiscountConfigs(); err != nil {
+	// 		log.Debug("Failed to reload discount configs", "error", err)
+	// 		return types.LFMDiscountConfig{}, false
+	// 	}
+	// }
+
+	fmt.Println("lmf.GetDiscountConfigByAddress discountConfigsReloadOnNextBlock", p.discountConfigsReloadOnNextBlock.Load())
+
+	fmt.Println("lmf.GetDiscountConfigByAddress", address)
 	p.lock.RLock()
+	fmt.Println("lmf.GetDiscountConfigByAddress1", p.discountConfigs)
 	config, ok = p.discountConfigs[address]
 	p.lock.RUnlock()
+	fmt.Println("lmf.GetDiscountConfigByAddres2", config, ok)
 	return config, ok
 }
 
