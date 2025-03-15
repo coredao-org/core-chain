e5c360d85616bea4aaec0f727a59a51b4d8ee0c4
momantech <cuimoman@qq.com>
2025-03-15 11:13:47 +0800

    chore: make function comment match function name
    Signed-off-by: momantech <cuimoman@qq.com>

--- metrics/sample.go
+++ metrics/sample.go
@@ -282,7 +282,7 @@ func SampleMin(values []int64) int64 {
 	return min
 }
 
-// SamplePercentiles returns an arbitrary percentile of the slice of int64.
+// SamplePercentile returns an arbitrary percentile of the slice of int64.
 func SamplePercentile(values []int64, p float64) float64 {
 	return SamplePercentiles(values, []float64{p})[0]
 }
