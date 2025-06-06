0a0ffc47fdff7e067adf02962d4fa34651dbe76b
Chris Ziogas <ziogaschr@gmail.com>
2025-06-06 09:57:48 +0300

    core/systemcontracts: configure Theseus upgrade on mainnet

--- core/systemcontracts/upgrade_test.go
+++ core/systemcontracts/upgrade_test.go
@@ -84,7 +84,7 @@ func TestUpgrade(t *testing.T) {
 		t.Error(err)
 	}
 
-	err = checkUpgradeConfig("https://raw.githubusercontent.com/coredao-org/core-genesis-contract/master/genesis.json", athenaUpgrade[mainNet])
+	err = checkUpgradeConfig("https://raw.githubusercontent.com/coredao-org/core-genesis-contract/master/genesis.json", theseusUpgrade[mainNet])
 	if err != nil {
 		t.Error(err)
 	}
