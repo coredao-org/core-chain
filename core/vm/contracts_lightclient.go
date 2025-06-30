10ef4ebe0a0b15222edd34d02645c60ad85fe08d
Chris Ziogas <ziogaschr@gmail.com>
2025-06-30 14:43:48 +0300

    core/vm: extract btcValidateV2 precompile to activate on Satoshi only
    - remove btcValidate as btcValidateV2 activates on genesis block for both Mainnet and Testnet

--- core/vm/contracts_lightclient.go
+++ core/vm/contracts_lightclient.go
@@ -15,54 +15,6 @@ const (
 	precompileContractInputMetaDataLength uint64 = 32
 )
 
-// btcValidate implemented as a precompiled contract.
-type btcValidate struct{}
-
-func (c *btcValidate) RequiredGas(input []byte) uint64 {
-	return params.BitcoinHeaderValidateBaseGas + uint64(len(input)/32)*params.BitcoinHeaderValidatePerWordGas
-}
-
-func (c *btcValidate) Run(input []byte) (result []byte, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("internal error: %v\n", r)
-		}
-	}()
-
-	if uint64(len(input)) <= precompileContractInputMetaDataLength {
-		return nil, fmt.Errorf("invalid input")
-	}
-
-	payloadLength := binary.BigEndian.Uint64(input[precompileContractInputMetaDataLength-uint64TypeLength : precompileContractInputMetaDataLength])
-	if uint64(len(input)) != payloadLength+precompileContractInputMetaDataLength {
-		return nil, fmt.Errorf("invalid input: input size should be %d, actual size is %d", payloadLength+precompileContractInputMetaDataLength, len(input))
-	}
-
-	rbuf := bytes.NewReader(input[precompileContractInputMetaDataLength:])
-	var mirror lightmirror.BtcLightMirror
-	err = mirror.Deserialize(rbuf)
-	if err != nil {
-		err = fmt.Errorf("deserialize btcLightMirror failed %s", err.Error())
-		return nil, err
-	}
-
-	// Verify MerkleRoot & coinbaseTx
-	err = mirror.CheckMerkle()
-	if err != nil {
-		err = fmt.Errorf("verify btcLightMirror failed %s", err.Error())
-		return nil, err
-	}
-
-	coinbaseAddr, addrType := mirror.GetCoinbaseAddress()
-	// result
-	// | coinbaseAddr        |
-	// | 20 bytes + 12 bytes |
-	addrTypeBytes := make([]byte, 4)
-	binary.BigEndian.PutUint32(addrTypeBytes, (uint32)(addrType))
-	addrTypeBytes = common.LeftPadBytes(addrTypeBytes[:], 32)
-	return append(common.RightPadBytes(coinbaseAddr[:], 32), addrTypeBytes...), nil
-}
-
 // btcValidateV2 implemented as a precompiled contract.
 type btcValidateV2 struct{}
 
