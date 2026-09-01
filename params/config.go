6644e07b0ab75343e124130b704cbbb0f462e2a8
ggcore0 <183680208+ggcore0@users.noreply.github.com>
2026-09-01 19:27:26 +0300

    chore: network upgrade v1.0.24

--- params/config.go
+++ params/config.go
@@ -183,6 +183,7 @@ var (
 		PascalTime:          newUint64(1764057600),
 		PragueTime:          newUint64(1764057600),
 		HermesTime:          newUint64(1764057600), // 2025-11-25 08:00:00 AM UTC
+		CoreRewardFixTime:   newUint64(1788397200), // 2026-09-03 01:00:00 AM UTC
 		Satoshi: &SatoshiConfig{
 			Period: 3,
 			Epoch:  200,
